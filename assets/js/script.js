4d5da35695c9225b60aba551816ce47b3b56190b
TCaldato <tcaldato@gmail.com>
2023-09-25 07:02:12 +0100

    Added attributions to JS code and improved some functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 /*jshint esversion: 6 */
 
 // Array of possible actions and winning combinations
+// Arrays created following the tutorial:
+//https://www.youtube.com/watch?v=lV2BMXdsDmc
 const actions = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 const userWinResults = ['scissorspaper', 'paperrock', 'rocklizard', 'lizardspock', 'spockscissors',
     'rockscissors', 'scissorslizard', 'lizardpaper', 'paperspock', 'spockrock'
@@ -11,6 +13,8 @@ let userChoice = '';
 let compChoice = '';
 
 // Elements from the DOM
+// Constants created following the tutorial:
+//https://www.youtube.com/watch?v=lV2BMXdsDmc
 const userChoiceElement = document.querySelector('.user-choice');
 const pickedElement = document.querySelector('.picked');
 const userPickElement = document.querySelector('.user-pick');
@@ -69,18 +73,27 @@ window.onclick = function(event) {
     }
 };
 
-// Add event listeners for each user choice and start the game when clicked
+// Function to handle card clicks
+function handleCardClick(event) {
+    const card = event.currentTarget;
+
+    // Adjust volume and play a sound effect
+    cardSound.volume = 0.2;
+    cardSound.play();
+    userChoice = getUserChoice(card);
+    compChoice = getComputerChoice();
+    startGame();
+}
+
+// Add click event listeners to all elements with the 'game-card' class
 document.querySelectorAll('.user-choice .game-card').forEach(card => {
-    card.addEventListener('click', (ev) => {
-        cardSound.volume = 0.2;
-        cardSound.play();
-        userChoice = getUserChoice(ev.target);
-        compChoice = getComputerChoice();
-        startGame();
-    });
+    // Attach the 'handleCardClick' function to the click event
+    card.addEventListener('click', handleCardClick);
 });
 
 // Function to start the game
+// Function created following the tutorial:
+//https://www.youtube.com/watch?v=lV2BMXdsDmc
 function startGame() {
     calculateWinner(userChoice, compChoice);
     userChoiceElement.classList.add('hidden');
@@ -91,11 +104,14 @@ function startGame() {
 }
 
 // Function to get user's choice
+// Function created following the tutorial:
+//https://www.youtube.com/watch?v=lV2BMXdsDmc
 function getUserChoice(target) {
-    if (target.nodeName === 'IMG') {
-        return target.parentElement.classList[1];
+    const elementWithClass = target.closest('.game-card');
+    if (elementWithClass) {
+        const className = elementWithClass.classList[1];
+        return className;
     }
-    return target.classList[1];
 }
 
 // Function to get computer's random choice
@@ -103,64 +119,83 @@ function getComputerChoice() {
     return actions[Math.floor(Math.random() * 5)];
 }
 
-// Function to calculate the winner and update the result
+// Function to calculate the winner and update the result on the score board
 function calculateWinner(user, comp) {
-    if (user === comp) {
-        resultTitleElement.innerText = 'Tie';
-    } else if (getUserWinsStatus(user + comp)) {
-        resultTitleElement.innerText = 'You Win';
-        userScore++;
-    } else {
-        resultTitleElement.innerText = 'You Lose';
-        compScore++;
+    const outcome = determineOutcome(user, comp);
+
+    switch (outcome) {
+        case 'Tie':
+            resultTitleElement.innerText = 'Tie';
+            break;
+        case 'You Win':
+            resultTitleElement.innerText = 'You Win';
+            userScore++;
+            break;
+        case 'You Lose':
+            resultTitleElement.innerText = 'You Lose';
+            compScore++;
+            break;
     }
 
     updateScoreBoard();
 
-    // Check if the game is over (either user or comp reached 5 points)
     if (userScore === 5 || compScore === 5) {
         const winner = userScore === 5 ? 'You Win the Game!' : 'Computer Wins the Game!';
         resultTitleElement.innerText = winner;
 
-        //Replace Play Again button to Reset Game button and change color to red
         const restartButton = resultElement.querySelector('button');
         restartButton.innerText = 'Reset Game';
         restartButton.style.color = 'red';
         restartButton.style.fontSize = '30px';
-        restartButton.removeEventListener('click', tryAgain);
+        restartButton.removeEventListener('click', playAgain);
         restartButton.addEventListener('click', resetGame);
 
-        // Remove the event listener for user choices to stop the game
         document.querySelectorAll('.user-choice .game-card').forEach(card => {
             card.removeEventListener('click', startGame);
         });
     }
+}
 
+//Function to determine if User win/lose or tie when choosing a card
+function determineOutcome(user, comp) {
+    if (user === comp) {
+        return 'Tie';
+    } else if (getUserWinsStatus(user + comp)) {
+        return 'You Win';
+    } else {
+        return 'You Lose';
+    }
 }
 
 // Function to check if the user wins based on the winning combinations
 function getUserWinsStatus(result) {
-    return userWinResults.some(winStr => winStr === result);
+    return userWinResults.includes(result);
 }
 
 // Function to create a choice element (either for user or computer) and add it to the DOM
 function buildChoiceElement(isItUserElement, className) {
-    const el = document.createElement('div');
-    el.classList.add(`game-card`, className);
-    el.innerHTML = `<img src="assets/images/index/${className}.gif" alt="${className}">`;
-    if (isItUserElement) {
-        userPickElement.appendChild(el);
-    } else {
-        pcPickElement.appendChild(el);
-    }
+    const container = isItUserElement ? userPickElement : pcPickElement;
+    const cardElemnt = createGameCardElement(className);
+    container.appendChild(cardElemnt);
+}
 
+function createGameCardElement(className) {
+    const cardElemnt = document.createElement('div');
+    cardElemnt.classList.add('game-card', className);
+    cardElemnt.innerHTML = `<img src="assets/images/index/${className}.gif" alt="${className}">`;
+    return cardElemnt;
 }
 
 // Function to handle the "Play Again" button click event
-function tryAgain() {
+// Function created following the tutorial:
+//https://www.youtube.com/watch?v=lV2BMXdsDmc
+function playAgain() {
     userChoiceElement.classList.remove('hidden');
     pickedElement.classList.add('hidden');
 }
+
+// Add an event listener for the "Play Again" button click
+resultElement.querySelector('button').addEventListener('click', playAgain);
 
 // Function to Reset the Game when the Score reaches 5 points
 function resetGame() {
@@ -170,6 +205,8 @@ function resetGame() {
 }
 
 // Function to clear the choice elements from the DOM before appending new ones
+// Function created following the tutorial:
+//https://www.youtube.com/watch?v=lV2BMXdsDmc
 function clearResultBeforeAppend() {
     userPickElement.innerHTML = '';
     pcPickElement.innerHTML = '';
@@ -202,6 +239,3 @@ function updateScoreBoard() {
         scoreCountElement1.style.color = 'black'; // Reset the color to default if it's not 5 yet
     }
 }
-
-// Add an event listener for the "Play Again" button click
-resultElement.querySelector('button').addEventListener('click', tryAgain);
